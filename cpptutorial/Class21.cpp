500f9f5d241e521a18a48c88430a010057617c76
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2018-09-24 09:47:58 +0700

    finish class

--- cpptutorial/Class21.cpp
+++ cpptutorial/Class21.cpp
@@ -17,6 +17,12 @@ public:
         return GPA > other.GPA;
     }
 };
+class Rectangle {
+  int width, height;
+public:
+  Rectangle(int x, int y) : width(x), height(y) {}
+  int area(void) { return width * height; }
+};
 double Student::getGPA(){ //namespace
     return GPA;
 }
@@ -37,6 +43,17 @@ int main(){
     sort(v.begin(),v.end());
     for (auto u:v)
         cout<< u.getGPA()<<" ";
-    //this is test for git
+    Rectangle obj (3, 4);
+    Rectangle * foo, * bar, * baz;
+    foo = &obj;
+    bar = new Rectangle (5, 6);
+   // baz = new Rectangle[2] { {2,5}, {3,6} };
+    cout << "obj's area: " << obj.area() << '\n';
+    cout << "*foo's area: " << (*foo).area() << '\n';
+    cout << "*bar's area: " << bar->area() << '\n';
+    //cout << "baz[0]'s area:" << baz[0].area() << '\n';
+    //cout << "baz[1]'s area:" << baz[1].area() << '\n';       
+    delete bar;
+    delete[] baz;
     return 0;
 }
