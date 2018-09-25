c2d3c979a15874d0d89c73c4476839e7fc1a9536
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2018-09-25 11:06:06 +0700

    .

--- grader/tempCodeRunnerFile.cpp
+++ grader/tempCodeRunnerFile.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void reverse(vector<int> &v, vector<int>::iterator a, vector<int>::iterator b)
-{   //v = 0 1 2 3 4 5 6  a = 2 b = 5
-    vector<int> k(a,b);
-    b--; //b = 4
-    for(int i=0;i<k.size()-1;i++){
-        v.insert(a,*b); // 0 1 4 2 3 4 5 6
-        v.erase(++b); //   0 1 4 2 3 5 6
-        a++;
-        b--;
+int main(){
+    int n,m;
+    cin>>n>>m; // 3 10
+    int q[n];
+    for(int i=0;i<n;i++)
+        { int a;cin>>a; q[i]=a;}
+    //for(auto i:q) cout<<i;
+    int mint = 0;
+    int pr = 0;
+    while(true){
+       for(auto i:q){
+            if(i!=0 and i>mint) continue;
+            if(i==0|| mint%i==0){
+                cout<<mint<<"\n";
+                pr+=1;
+                if(pr==m) 
+                {break;}
+            }
+        if(pr==m) 
+                {break;}
+        }
+    if(pr==m) 
+                {break;}
+    ++mint;
     }
-       
-}
-int main()
-{
-    //read input
-    int n, a, b;
-    cin >> n;
-    vector<int> v;
-    for (int i = 0; i < n; i++)
-    {
-        int c;
-        cin >> c;
-        v.push_back(c);
-    }
-    cin >> a >> b;
-    //call function
-    reverse(v, v.begin() + a, v.begin() + b + 1);
-    //display content of the vector
-    for (auto &x : v)
-    {
-        cout << x << " ";
-    }
-    cout << endl;
+    return 0;
 }
