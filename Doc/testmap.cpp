874b656aff9f6095c62eecc56e6c45154b41e0f4
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2018-09-29 09:04:45 +0700

    ///

--- Doc/testmap.cpp
+++ Doc/testmap.cpp
@@ -1,8 +1,28 @@
-#include <iostream>
-#include <map>
+// #include <iostream>
+// #include <map>
+// using namespace std;
+// int main(){
+//     map<string,int> d;
+//     d["a"] = 20;
+//     cout<<d["a"];
+// }
+#include<iostream>
+#include<vector>
+#include<sstream>
 using namespace std;
-int main(){
-    map<string,int> d;
-    d["a"] = 20;
-    cout<<d["a"];
+vector<string> split(const string &s, char delim) {
+    vector<string> result;
+    stringstream ss (s);
+    string item;
+    while (getline(ss, item, delim)) {
+        result.push_back(item);
+    }
+    return result;
+}
+
+int main() {
+    string str = "adsf+qwer+poui+fdgh";
+    vector<string> v = split(str, '+');
+    for (auto i : v) cout << i << endl;
+    return 0;
 }
