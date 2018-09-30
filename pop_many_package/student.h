1bba9218ec4358e9068f2e969ccc6c8beb17c2a3
Karnkitti Kittikamron <karnkitti_tang@hotmail.com>
2018-09-30 16:37:35 +0700

    ..

--- pop_many_package/student.h
+++ pop_many_package/student.h
@@ -5,13 +5,28 @@
 template <typename T>
 void CP::stack<T>::multi_pop(size_t K) {
   //write your code here
+  if(K > mSize) K=mSize;
+  for(size_t i=0;i<K;i++){
+   pop();
+  }
 }
 
 template <typename T>
 std::stack<T> CP::stack<T>::remove_top(size_t K) {
   //write your code here
-  //
+  std::stack<T> a,b;
+  if(K > mSize) K=mSize;
+  for(size_t i=0;i<K;i++){
+    a.push(top());
+    pop();
+  }
+  while(!a.empty()){
+    b.push(a.top());
+    a.pop();
+  }
+
   //don't forget to return an std::stack
+  return b;
 }
 
 #endif
