4b148d9d0f44e909fc3268c761d7ca8f8b1165be
karnkittik <karnkitti_tang@hotmail.com>
2018-12-07 14:42:49 +0700

    review hash

--- cpptutorial/Hash31.cpp
+++ cpptutorial/Hash31.cpp
@@ -5,11 +5,16 @@ int main(){
     hash<string> hStr;
     hash<float> hFloat;
     hash<int> hInt;
+    hash<string> kStr;
+    hash<int> kInt;
+    hash<float> kFloat;
 
     cout<< hStr("Kris") << endl;
     cout<< hFloat(4.00f) << endl;
     cout<< hInt(25) << endl;
-
+    cout<< kStr("Kris") << endl;
+    cout<< kFloat(4.00f) << endl;
+    cout<< kInt(25) << endl;
     cout<< hash<string>()("Kris") << endl;
     cout<< hash<float>()(4.00f) << endl;
     cout<< hash<int>()(25) << endl;
